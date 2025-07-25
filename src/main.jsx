6ecf8a204b0bc1b3980675733516ccb72628baf9
Dilip Chandu Bethu <dileepbethu111@gmail.com>
2025-07-25 18:18:03 +0530

    Deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
+import { HashRouter  } from 'react-router-dom'; // ✅ Add this
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter > 
+  < HashRouter > 
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
